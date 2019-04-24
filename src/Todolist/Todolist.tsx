13d37d458015267f999c070245a9fa7015504d9f
heym <heym@hsmap.com>
2019-04-24 17:18:21 +0800

    import react

--- src/Todolist/Todolist.tsx
+++ src/Todolist/Todolist.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react';
-import { Component } from 'react';
+import React, { Component } from 'react';
 import Add from './components/Add';
 import Todos from './components/Todos';
 import Filter from './components/Filter';
